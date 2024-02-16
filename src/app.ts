cfd4957704dff5c203ceac7b7da31ff0e2bea193
Nevac <cagatayoezyurt@gmail.com>
2024-02-16 22:06:14 +0100

    trust proxy config

--- src/app.ts
+++ src/app.ts
@@ -41,6 +41,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.enable('trust proxy')
 
 var corsOptions = {
     credentials: true,
